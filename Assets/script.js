ab1e50925d0c73af8103f1f6018d539a7023863d
Afif Patel <afifpatel@hotmail.co.uk>
2022-07-23 17:18:05 +0100

    high-scores html created and linked

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+var startQuizButton = document.querySelector("#start-quiz-btn");
+startQuizButton.addEventListener("click", startquizbutton);
+
+function startquizbutton() {
+  //remove section
+  //start timer //
+}
