531e56200121c45845c88983308c69b5436e6cb9
Afif Patel <afifpatel@hotmail.co.uk>
2022-07-24 15:51:32 +0100

    test

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
 var startQuizButton = document.querySelector("#start-quiz-btn");
 startQuizButton.addEventListener("click", startQuizBtn);
+var timerSection = document.getElementById("timer-section");
+var questionSection = document.getElementById("question-section");
 
 function startQuizBtn() {
   //remove section
@@ -7,7 +9,21 @@ function startQuizBtn() {
   startQuizSection.remove();
 
   //start timer //
-  //build question section
 
-  //render first question
+  //build timer section
+
+  var timerContainer = document.createElement("p");
+  timerSection.textContent = "Time remaining : 60 ";
+  timerSection.appendChild(timerContainer);
+
+  //build question section
+  var questionContainer = document.createElement("h1");
+  questionSection.textContent = "Which number is the greatest? ";
+  questionSection.appendChild(questionContainer);
+
+  //build answer section
+  var li1 = document.createElement("li");
+  li1.textContent = "10";
+  document.body.children[1].children[2].appendChild(li1);
 }
+console.log(document.body.children[1].children[2]);
