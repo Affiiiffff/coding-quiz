0943194877cf672363852bbaf8554d8f001c64b4
Afif Patel <afifpatel@hotmail.co.uk>
2022-07-31 17:03:32 +0100

    submit linked to highscorep page

--- Assets/highscorescript.js
+++ Assets/highscorescript.js
@@ -1,11 +1,12 @@
 var highscoreNames = document.getElementById("names");
 var timeTaken = document.getElementById("scoresdiv");
 var displayScore = JSON.parse(localStorage.getItem("new-score"));
+
 var namesList = "";
 var scoresList = "";
 for (let i = 0; i < displayScore.length; i++) {
-  namesList += displayScore[i].name;
-  scoresList += displayScore[i].time;
+  namesList += displayScore[i].name + "\n";
+  scoresList += displayScore[i].time + "\n";
 }
 
 highscoreNames.textContent = namesList;
